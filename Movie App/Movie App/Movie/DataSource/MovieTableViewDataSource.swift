3fa625dc9a2feaa8984cdffb4b793cbd7c5496d3
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 01:04:10 +0800

    implement data source

--- Movie App/Movie App/Movie/DataSource/MovieTableViewDataSource.swift
+++ Movie App/Movie App/Movie/DataSource/MovieTableViewDataSource.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+class MovieTableViewDataSource<CELL : UITableViewCell,T> : NSObject, UITableViewDataSource {
+    
+    private var cellIdentifier : String!
+    private var items : T!
+    var configureCell : (CELL, Int) -> () = {_,_ in }
+    
+    
+    init(cellIdentifier : String, configureCell : @escaping (CELL, Int) -> ()) {
+        self.cellIdentifier = cellIdentifier
+        self.configureCell = configureCell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? CELL else { return UITableViewCell() }
+
+        self.configureCell(cell, indexPath.item)
+        return cell
+    }
+    
+}
