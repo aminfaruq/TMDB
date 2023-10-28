e6349559b23799def524804ff2d80f829d3ac73d
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 03:20:02 +0800

    revamp viewModel

--- Movie App/Movie App/Movie/ViewModel/MovieVM.swift
+++ Movie App/Movie App/Movie/ViewModel/MovieVM.swift
@@ -6,25 +6,40 @@
 //
 
 import Foundation
-import UIKit
 import Combine
 
+enum ContentType {
+    case movie
+    case tvSeries
+}
 
-class MovieVM : NSObject {
+class MovieViewModel: NSObject {
     private let apiService: APIService
+    private var cancellables = Set<AnyCancellable>()
+    var contentType: ContentType
     
     @Published var nowPlayingResponse: MovieResponse?
     @Published var popularResponse: MovieResponse?
     @Published var topRatedResponse: MovieResponse?
     @Published var upcomingResponse: MovieResponse?
+    @Published var airingTodayResponse: MovieResponse?
+    @Published var onTheAirResponse: MovieResponse?
     
-    private var cancellables = Set<AnyCancellable>()
-    
-    init(apiService: APIService) {
+    init(apiService: APIService, contentType: ContentType) {
         self.apiService = apiService
+        self.contentType = contentType
     }
     
-    func callFuncToGetMovieData() {
+    func fetchData() {
+        switch contentType {
+        case .movie:
+            fetchMovies()
+        case .tvSeries:
+            fetchTVSeries()
+        }
+    }
+    
+    private func fetchMovies() {
         apiService.fetchNowPlayingMovies()
             .sink(receiveCompletion: { _ in }) { [weak self] empData in
                 self?.nowPlayingResponse = empData
@@ -46,6 +61,32 @@ class MovieVM : NSObject {
         apiService.fetchUpcomingMovies()
             .sink(receiveCompletion: { _ in }) { [weak self] empData in
                 self?.upcomingResponse = empData
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func fetchTVSeries() {
+        apiService.fetchAiringTodaySeries()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.airingTodayResponse = empData
+            }
+            .store(in: &cancellables)
+        
+        apiService.fetchOnTheAirSeries()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.onTheAirResponse = empData
+            }
+            .store(in: &cancellables)
+        
+        apiService.fetchPopularSeries()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.popularResponse = empData
+            }
+            .store(in: &cancellables)
+        
+        apiService.fetchTopRatedSeries()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.topRatedResponse = empData
             }
             .store(in: &cancellables)
     }
