41629d81a81763c849e1850fced7015f19091717
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 00:43:12 +0800

    implement tabbar

--- Movie App/Movie App/ViewController.swift
+++ Movie App/Movie App/ViewController.swift
@@ -7,11 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.configureTab()
+    }
+    
+    func configureTab() {
+        let movieVC = MainTabBarWireframe.initHome()
+        let rootMovieVC = UINavigationController(rootViewController: movieVC)
+        setupTabItem(
+            rootMovieVC,
+            title: "Movie",
+            unselectedImage: "film",
+            selectedImage: "film.fill")
+        
+        let tvVC = UIViewController()
+        let rootTvVC = UINavigationController(rootViewController: tvVC)
+        setupTabItem(
+            rootTvVC,
+            title: "TV",
+            unselectedImage: "tv",
+            selectedImage: "tv.fill")
+
+        let personVC = UIViewController()
+        let rootPersonVC = UINavigationController(rootViewController: personVC)
+        setupTabItem(
+            rootPersonVC,
+            title: "Person",
+            unselectedImage: "person",
+            selectedImage: "person.fill")
+        
+        let searchVC = UIViewController()
+        let rootExploreVC = UINavigationController(rootViewController: searchVC)
+        setupTabItem(
+            rootExploreVC,
+            title: "Search",
+            unselectedImage: "magnifyingglass",
+            selectedImage: "magnifyingglass")
+        
+        self.viewControllers = [rootMovieVC, rootTvVC, rootPersonVC, rootExploreVC]
+    }
+    
+    private func setupTabItem(_ base: UINavigationController, title: String, unselectedImage: String, selectedImage: String) {
+        base.tabBarItem = UITabBarItem(
+            title: title,
+            image: nil,
+            selectedImage: nil)
+        
+        base.tabBarItem.image = tabBarImageFrom(UIImage(systemName: unselectedImage)?.withRenderingMode(.alwaysOriginal))
+        base.tabBarItem.selectedImage = tabBarImageFrom(UIImage(systemName: selectedImage)?.withRenderingMode(.alwaysOriginal))
     }
 
 
