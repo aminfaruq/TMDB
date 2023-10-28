27988270e76a940b61024d7b78c2ceb41b9a0118
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 01:04:23 +0800

    implement viewModel

--- Movie App/Movie App/Movie/ViewModel/MovieVM.swift
+++ Movie App/Movie App/Movie/ViewModel/MovieVM.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import UIKit
+import Combine
+
+
+class MovieVM : NSObject {
+    private let apiService: APIService
+    
+    @Published var nowPlayingResponse: MovieResponse?
+    @Published var popularResponse: MovieResponse?
+    @Published var topRatedResponse: MovieResponse?
+    @Published var upcomingResponse: MovieResponse?
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(apiService: APIService) {
+        self.apiService = apiService
+    }
+    
+    func callFuncToGetMovieData() {
+        apiService.fetchNowPlayingMovies()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.nowPlayingResponse = empData
+            }
+            .store(in: &cancellables)
+        
+        apiService.fetchPopularMovies()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.popularResponse = empData
+            }
+            .store(in: &cancellables)
+        
+        apiService.fetchTopRatedMovies()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.topRatedResponse = empData
+            }
+            .store(in: &cancellables)
+        
+        apiService.fetchUpcomingMovies()
+            .sink(receiveCompletion: { _ in }) { [weak self] empData in
+                self?.upcomingResponse = empData
+            }
+            .store(in: &cancellables)
+    }
+    
+}
