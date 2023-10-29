9bffcf13efcf2a0c73acd8f64df9c2568716da47
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 20:33:50 +0800

    implement model

--- Movie App/Movie App/Model/ActorModel.swift
+++ Movie App/Movie App/Model/ActorModel.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+struct ActorResponse: Codable {
+    let page: Int?
+    let results: [Actor]?
+    let totalPages: Int?
+    let totalResults: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+struct Actor: Codable {
+    let adult: Bool?
+    let gender: Int?
+    let id: Int?
+    let knownFor: [KnownFor]?
+    let knownForDepartment: String?
+    let name: String?
+    let popularity: Double?
+    let profilePath: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case gender
+        case id
+        case knownFor = "known_for"
+        case knownForDepartment = "known_for_department"
+        case name
+        case popularity
+        case profilePath = "profile_path"
+    }
+}
+
+struct KnownFor: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIds: [Int]?
+    let id: Int?
+    let mediaType: String?
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case id
+        case mediaType = "media_type"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
