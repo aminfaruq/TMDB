b4ad0d95dea38626c5186710a09eb39b04b55ba6
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 20:37:02 +0800

    make logic just from cell

--- Movie App/Movie App/Person/Views/PersonViewCell.swift
+++ Movie App/Movie App/Person/Views/PersonViewCell.swift
@@ -6,18 +6,40 @@
 //
 
 import UIKit
+import Combine
 
 class PersonViewCell: UITableViewCell {
+
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personKnowAsLabel: UILabel!
+    @IBOutlet weak var cardView: UIView!
+    
+    private var cancellables: Set<AnyCancellable> = []
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func setupView(data: Actor) {
+        cardView.backgroundColor = UIColor(hex: "F5F7F8")
+        cardView.setCornerRadius(radius: 12)
+        personNameLabel.text = data.name
+        personImage.image = nil
 
-        // Configure the view for the selected state
+        let knownForTitles = data.knownFor?.compactMap { $0.originalTitle } ?? []
+        personKnowAsLabel.text = knownForTitles.joined(separator: ", ")
+
+        ImageHelper.getImagePublisher(url: data.profilePath ?? "")
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: \(error)")
+                }
+            }, receiveValue: { image in
+                self.personImage.image = image
+            })
+            .store(in: &cancellables)
     }
     
 }
