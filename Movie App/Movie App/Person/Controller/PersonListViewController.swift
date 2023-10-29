41b908dce6d1006f05fb48407104e18b700e7cde
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-29 20:36:42 +0800

    implement feature list persons

--- Movie App/Movie App/Person/Controller/PersonListViewController.swift
+++ Movie App/Movie App/Person/Controller/PersonListViewController.swift
@@ -6,24 +6,64 @@
 //
 
 import UIKit
+import Combine
 
 class PersonListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var viewModel: PersonViewModel!
+    private var dataSource: PersonTableViewDataSource<PersonViewCell, Actor>!
+    private var delegate: PersonTableViewDelegate!
+    private var cancellables: Set<AnyCancellable> = []
+
+    var currentPage = 1
+    var totalPages = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
+        setupViewModel()
+        bindViewModel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTableView() {
+        tableView.register(PersonViewCell.nib(), forCellReuseIdentifier: PersonViewCell.identifier())
     }
-    */
-
+    
+    @objc private func loadMoreData() {
+        currentPage += 1
+        bindViewModel()
+    }
+    
+    private func bindViewModel() {
+        viewModel.fetchPersons(page: currentPage, completion: {[weak self] (actorResponse) in
+            self?.viewModel.actorResponse = actorResponse
+            self?.updateDataSource()
+            self?.totalPages = actorResponse?.totalPages ?? 1
+        })
+    }
+    
+    private func setupViewModel() {
+        viewModel = PersonViewModel(apiService: APIService<ActorResponse>())
+    }
+    
+    private func updateDataSource() {
+        delegate = PersonTableViewDelegate(height: 120, configureWillDisplayCell: {
+            if ((self.tableView.contentOffset.y + self.tableView.frame.size.height) >= self.tableView.contentSize.height) {
+                if self.currentPage < self.totalPages {
+                    self.loadMoreData()
+                }
+            }
+           
+        })
+        dataSource = PersonTableViewDataSource(cellIdentifier: PersonViewCell.identifier(), items: self.viewModel.actorResponse?.results ?? [], configureCell: { (cell, data) in
+            cell.setupView(data: data)
+        })
+        
+        DispatchQueue.main.async {
+            self.tableView.dataSource = self.dataSource
+            self.tableView.delegate = self.delegate
+            self.tableView.reloadData()
+        }
+    }
 }
